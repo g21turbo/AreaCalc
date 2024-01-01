cae4d05467c125a6adee82029cffb658fe4b3614
g21turbo <dg21stew@gmail.com>
2023-12-31 22:21:19 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Square square = new Square(5, 5);
+
+        System.out.println("the area of the square is " + square.area());
+
+
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,18 @@
+public class Square {
+
+    int length;
+
+    int width;
+
+    public Square(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int area() {
+        return length * width;
+    }
+
+
+
+}
